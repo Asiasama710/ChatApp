498873316cb2c71aa748d03f660628ecfdbbe44b
Asia sama <sehunexo710@gmail.com>
2024-03-11 21:37:47 +0300

    create MessageCard for resever and sender

---
+++ app/src/main/java/com/asiasama/chatapp/ui/screen/chat/composable/MessageCard.kt
@@ -0,0 +1,71 @@
+package com.asiasama.chatapp.ui.screen.chat.composable
+
+import android.os.Build
+import android.text.Html
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.asiasama.chatapp.ui.screen.chat.MessageUIState
+import com.asiasama.chatapp.ui.theme.Theme
+
+@Composable
+fun MessageCard(
+    message: MessageUIState,
+    modifier: Modifier = Modifier,
+) {
+
+    Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 4.dp)
+                .then(modifier),
+            contentAlignment = if (message.isMe) Alignment.CenterEnd else Alignment.CenterStart,
+    ) {
+
+        Text(
+                modifier = Modifier
+                    .background(
+                            color = if (message.isMe) {
+                                Theme.colors.primary
+                            } else {
+                                Theme.colors.onPrimary
+                            },
+                            shape = RoundedCornerShape(
+                                    topStart = 8.dp,
+                                    topEnd = 8.dp,
+                                    bottomStart = if (message.isMe) 8.dp else 0.dp,
+                                    bottomEnd = if (!message.isMe) 8.dp else 0.dp
+                            )
+                    )
+                    .padding(16.dp)
+                ,
+                text = message.message.parseHtml(),
+                style = Theme.typography.bodyMedium,
+                textAlign = if (message.isMe) {
+                    TextAlign.End
+                } else {
+                    TextAlign.Start
+                },
+                color = if (message.isMe) {
+                    Theme.colors.onPrimary
+                } else {
+                    Theme.colors.contentPrimary
+                },
+        )
+    }
+}
+fun String.parseHtml(): String {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
+    } else {
+        Html.fromHtml(this).toString()
+    }
+}
